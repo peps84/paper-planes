82f93a3d3f77ecc3f3d8f4a9c4419e0a8b202424
Sam Pepper <sam.pepper@adylic.com>
2019-03-15 08:42:56 +0000

    More webpack setup / practice

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import paper from 'paper';
+import './style.css';
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
   }
